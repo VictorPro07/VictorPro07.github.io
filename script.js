a00054317eea7438d2b51d692f68054f81be532f
Victor <victor.hu.pro@gmail.com>
2025-03-31 00:09:56 +0200

    Update Info

--- script.js
+++ script.js
@@ -89,7 +89,7 @@
 
     // Effet machine à écrire
     document.addEventListener('DOMContentLoaded', () => {
-        const text = "Diplômé de l’ecole d’ingénieur IMT Nord Europe (Mines de Douai), j'ai suivi une formation rigoureuse en science des données combinée à des expériences terrain concrètes. Aujourd’hui, je recherche un poste dans le domaine de la Data.";
+        const text = "Diplômé de l’ecole d’ingénieur IMT Nord Europe (Mines de Douai), j'ai suivi une formation rigoureuse en science des données combinée à des expériences terrain concrètes.";
         const typewriter = document.querySelector('.typewriter');
         
         // Effacer le texte initial
